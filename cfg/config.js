d8390883fd9e2e0b3684fa2794e8cf070f11aa3e
Cameron Edwards <edwards.cameron.pc@gmail.com>
2017-01-19 22:06:43 -0600

    Point to a hosted database on modulus.io

--- cfg/config.js
+++ cfg/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     secret: 'testSecret',
-    dbUrl: 'mongodb://localhost:27017/iForgot'
+    //dbUrl: 'mongodb://localhost:27017/iForgot',
+    dbUrl: 'mongodb://user1:pass1@jello.modulusmongo.net:27017/uMemer8e'
 };
